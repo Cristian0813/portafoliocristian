d1049ddbe73445bf4d517d8f19f2f49e1b2e301a
Cristian Arias <javier.0813@hotmail.com>
2024-09-18 22:09:18 -0500

    Isotipo CA

--- src/shared/common/Isotipo.tsx
+++ src/shared/common/Isotipo.tsx
@@ -8,7 +8,7 @@ export default function Isotipo() {
         <span className="sr-only">Cristian Arias</span>
         <Image
           alt="Cristian Arias"
-          src="/IsotipoCA.svg"
+          src="/IsoCA.svg"
           className="mx-auto md:mx-0 hover:opacity-80 transition-opacity duration-300 dark:invert dark:brightness-0 hover:dark:invert-0 hover:dark:brightness-150"
           width={150}
           height={150}
